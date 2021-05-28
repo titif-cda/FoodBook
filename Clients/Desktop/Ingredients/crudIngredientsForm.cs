4a2cfbc1bb39ce1e7dda0e7cefd3782a1f335041
titif-cda <cda3elji@gmail.com>
2021-05-28 11:53:35 +0200

    test modif

--- Clients/Desktop/Ingredients/crudIngredientsForm.cs
+++ Clients/Desktop/Ingredients/crudIngredientsForm.cs
@@ -15,6 +15,7 @@ namespace Desktop.Ingredients
     public partial class crudIngredientsForm : Form
     {
         Ingredient CurrentIngredient;
+
         public bool isCreation = false;
         private readonly IRestaurantService _restaurantService;
     
